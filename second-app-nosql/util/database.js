7665f5bbd901cc480febd4bb95918cf7f9d084b7
Soumyanil Das <dassic@outlook.com>
2022-02-09 20:12:18 +0530

    Added MongoDb connection

--- second-app-nosql/util/database.js
+++ second-app-nosql/util/database.js
@@ -4,7 +4,7 @@ const mongoClient = mongodb.MongoClient;
 const mongoConnect = callback => {          // connecting to the mongodb server
     mongoClient.connect('mongodb+srv://dassic:FRFdAaxfT4WNYkPe@cluster0.ad9yl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
         .then((client) => {
-            console.log("Conneted!");
+            console.log("Connected!");
             callback(client);
         })
         .catch(err => {
