14868c0dfc882f856badb995ef6d8c22587d24e5
Soumyanil Das <dassic@outlook.com>
2023-02-11 15:45:56 +0530

    async requests added on del prod|element del !=reload

--- second-app-nosql/controllers/admin.js
+++ second-app-nosql/controllers/admin.js
@@ -173,8 +173,8 @@ exports.getProducts = (req, res, next) => {
         });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
         .then(product => {
             if (!product) {
@@ -185,12 +185,18 @@ exports.postDeleteProduct = (req, res, next) => {
             return Product.deleteOne({ _id: prodId, userId: req.user._id });
         })
         .then(() => {
-            // console.log('DESTROYED PRODUCT');
-            res.redirect('/admin/products');
+            console.log('DESTROYED PRODUCT');
+            // res.redirect('/admin/products');
+            res.status(200).json({
+                message: "Success!"
+            })
         })
         .catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            next(error);
+            // const error = new Error(err);
+            // error.httpStatusCode = 500;
+            // next(error);
+            res.status(500).json({
+                message: "Failed!"
+            })
         });
 };
